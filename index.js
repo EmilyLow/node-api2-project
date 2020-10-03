d6be6c5d701dc97db392064e894a2a94addd64cd
EmilyLow <emlow@me.com>
2020-10-03 14:14:57 -0500

    postsr-router exists

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //Basic code to get it running
 const express = require('express');
 
+//not sure about db
+const postsDB = require("./data/db");
+const postsRouter = require("./posts/posts-router");
+
 const server = express();
+const port = 9000;
+server.use(express.json()) 
+server.use(postsRouter);
 
-server.use('/', (req, res) => res.send('API up and running!'));
 
-// using port 9000 for this example
-server.listen(9000, () => console.log('API running on port 9000'));
+server.use('/', (req, res) => res.send('API up and running! Confirmed.'));
 
-//!! Why do you sometimes do 'npm start' and 'npm run server'. Is that due to package.json code?
+
+server.listen(port, () => console.log('API running on port 9000'));
+
+//!! Why do you sometimes do 'npm start' and 'npm run server' and sometimes 'node index.js'. Is that due to package.json code?
+//It seemds like 'npm start' and 'npm run server' might need to be defined inside the "scripts" part of package.son. E.g. "start" : "node index.js" For everything but start/test need to put "run" which is why "npm run server" works. 
---
+++ posts/posts-router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+/* 
+    router.get("/posts", req, res) => {
+
+    })
+
+
+*/
+
+
+
+
+module.exports = router
