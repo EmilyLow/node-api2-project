c193f524cf9e3791cd589bbb4274e055eab8d147
EmilyLow <emlow@me.com>
2020-10-03 15:59:52 -0500

    post returns correctly

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -14,6 +14,44 @@ router.get("/posts", (req, res) => {
         })
  })
 
+ router.post("/posts", (req, res) => {
+    if(!req.body.title || !req.body.contents) {
+        //!!When do you use return and when is it automatic?
+       res.status(400).json({errorMessage: "Please provide title and contents for the post."
+    })
+    }
+
+    console.log("req.body", req.body)
+
+    posts.insert(req.body)
+        .then((post) => {
+            //This was a roundabout way to return the actual post and not just the id. Was there a better way?
+            let postContent;
+            console.log("test console");
+            posts.findById(post.id)
+            .then((result) => {
+                res.status(201).json(result);
+            } )
+            .catch(( (error) => 
+                res.status(500).json({
+                error: "There was an error while saving the post to the database after posting but while locating." 
+            })
+
+            ))
+            
+            
+        })
+        .catch((error) => {
+            console.log(error, "Insert error");
+            //! Does this count as returning the json?
+            res.status(500).json({
+                 error: "There was an error while saving the post to the database" 
+            })
+        })
+
+
+ })
+
 
 
 
